cc114a0ad03b17fcd951cd53c4da7b7c1e208bba
Wendy Afrim <wendy.afrim2@gmail.com>
2023-05-10 18:49:29 +0200

    feat(crud): Generating tasks and slots crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { SlotsModule } from './slots/slots.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule, SlotsModule],
   controllers: [AppController],
   providers: [AppService],
 })
